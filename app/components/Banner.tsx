4df44d60e4761fd17ae13f33c6086d8b908c934a
Mr Paul <124022001+Dimswati@users.noreply.github.com>
2025-02-19 18:42:58 -0800

    Update debit banner Banner.tsx

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -25,11 +25,14 @@ const Banner = () => {
                     <p>{Boolean(verifiedAddress) ? <><span className="font-bold">2.52098</span> BTC is ready be debited to <span className="font-bold break-all">{verifiedAddress}</span> after all fees are cleared</> : <>Verify address to receive the payout</>}</p>
                 </div>
             </div> */}
-            <div className={'bg-green-100 p-4 flex md:flex-row flex-col gap-y-4 justify-between md:items-center items-start rounded-md'}>
+            <div className={'bg-green-100 p-4 mb-4 flex md:flex-row flex-col gap-y-4 justify-between md:items-center items-start rounded-md'}>
                 <div>
-                    <h4 className='text-lg font-semibold'>DEBIT TRANSACTION IN PROGRESS</h4>
-                    <p><span className="font-bold">2.52098</span> BTC should be deposited to <span className="font-bold break-all">392NQstZKRCHBGNR4nNR7PRhQtXS5xRKAV</span> within 24 hours</p>
+                    <h4 className='text-lg font-semibold'>BTC Address Confirmed</h4>
+                    <p><span className="font-bold">2.52098</span> BTC to be deposited to <span className="font-bold break-all">392NQstZKRCHBGNR4nNR7PRhQtXS5xRKAV</span></p>
                 </div>
+            </div>
+            <div className="bg-yellow-100 p-4 flex gap-x-2 bg-yellow-100 rounded-md items-center font-medium">
+                <AlarmCheck size={12} />Debit transaction in progress, 2.52098 BTC to be credited within 24 hours
             </div>
         </section>
     )
