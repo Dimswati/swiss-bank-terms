2a312598b689d336126925449b1e03ef16f564d5
Mr Paul <124022001+Dimswati@users.noreply.github.com>
2025-02-19 16:08:21 -0800

    Update LoginForm.tsx

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -16,7 +16,7 @@ type FormProps = {
 const LoginForm = () => {
 
   const username = "johnsoliday"
-  const token = "dMcDmwkJrJ1YAp0iiRlGcF2qa"
+  const token = "YxF7CVf0bL0KOHGDHSl3n3oZA"
 
   const router = useRouter()
   const [error, setError] = useState<string>()
